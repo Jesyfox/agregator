5eb0166689de9962f8e180938174327d69ff9834
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-12 16:26:46 +0300

    feature: added post list page

--- site_agregator/post_previewer/views.py
+++ site_agregator/post_previewer/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import View
 
+from site_parser.models import Post
+
 
 class Index(View):
     template_name = 'index.html'
+    data = {'posts': Post.objects.all()}
 
     def get(self, request):
-        return render(request, self.template_name)
+        return render(request, self.template_name, context=self.data)
