4cdb9d527acd688f9af7fc59f6a13e8430f5fbff
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-12 16:51:38 +0300

    feature: added post detail view

--- site_agregator/post_previewer/urls.py
+++ site_agregator/post_previewer/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index')
+    path('', views.Index.as_view(), name='index'),
+    path('<int:pk>', views.PostDetail.as_view(), name='post_detail')
 ]
--- site_agregator/post_previewer/views.py
+++ site_agregator/post_previewer/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
 
 from site_parser.models import Post
@@ -10,3 +10,11 @@ class Index(View):
 
     def get(self, request):
         return render(request, self.template_name, context=self.data)
+
+
+class PostDetail(View):
+    template_name = 'post_detail.html'
+
+    def get(self, request, pk):
+        data = {'post': get_object_or_404(Post, pk=pk)}
+        return render(request, self.template_name, context=data)
