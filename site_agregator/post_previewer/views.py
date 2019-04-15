74ef29f18786cbcdcef9fca49dd466979478a3a3
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-15 14:58:28 +0300

    feature: added search by title

--- site_agregator/post_previewer/views.py
+++ site_agregator/post_previewer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic import View
+from django.core.exceptions import ImproperlyConfigured
 
 from tagging.views import TaggedObjectList
 
@@ -12,15 +13,33 @@ class PostTagDetail(TaggedObjectList):
     paginate_by = 20
     template_name = 'post_list.html'
 
+    def get_queryset_or_model(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(title__icontains=query)
+        elif self.model is not None:
+            return self.model
+        else:
+            raise ImproperlyConfigured(
+                "%(cls)s is missing a QuerySet. Define "
+                "%(cls)s.model, %(cls)s.queryset, or override "
+                "%(cls)s.get_queryset_or_model()." % {
+                    'cls': self.__class__.__name__
+                }
+            )
+
 
 class PostListView(ListView):
     model = Post
     paginate_by = 20
     template_name = 'post_list.html'
 
-    def get_context_data(self, **kwargs):
-        context = super(ListView, self).get_context_data(**kwargs)
-        return context
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Post.objects.filter(title__icontains=query)
+        else:
+            return Post.objects.all()
 
 
 class PostDetail(View):
