5e9790f15f2918b828f8ea724181956e098380c9
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-15 14:30:01 +0300

    feature: added tag filter

--- site_agregator/post_previewer/urls.py
+++ site_agregator/post_previewer/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
+from django.urls import path, re_path
+
+from tagging.views import TaggedObjectList
+from site_parser.models import Post
 
 from . import views
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
-    path('<int:pk>', views.PostDetail.as_view(), name='post_detail')
+    path('<int:pk>', views.PostDetail.as_view(), name='post_detail'),
+    path('/tag/<str:tag>', views.PostTagDetail.as_view(), name='post_tag_detail')
 ]
--- site_agregator/post_previewer/views.py
+++ site_agregator/post_previewer/views.py
@@ -2,9 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic import View
 
-from tagging.models import TaggedItem
+from tagging.views import TaggedObjectList
 
 from site_parser.models import Post
+
+
+class PostTagDetail(TaggedObjectList):
+    model = Post
+    paginate_by = 20
+    template_name = 'post_list.html'
 
 
 class PostListView(ListView):
@@ -15,13 +21,6 @@ class PostListView(ListView):
     def get_context_data(self, **kwargs):
         context = super(ListView, self).get_context_data(**kwargs)
         return context
-
-    def get_queryset(self):
-        query = self.request.GET.get('q')
-        if query:
-            return TaggedItem.objects.get_by_model(Post, query)  # ['house', 'thing'])
-        else:
-            return Post.objects.all()
 
 
 class PostDetail(View):
