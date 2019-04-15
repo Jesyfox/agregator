3d02a0bf910afeebfb92872c46754df7bc4ad6ed
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-15 15:04:09 +0300

    fix:slash querystring

--- site_agregator/post_previewer/urls.py
+++ site_agregator/post_previewer/urls.py
@@ -8,5 +8,5 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:pk>', views.PostDetail.as_view(), name='post_detail'),
-    path('/tag/<str:tag>', views.PostTagDetail.as_view(), name='post_tag_detail')
+    path('tag/<str:tag>', views.PostTagDetail.as_view(), name='post_tag_detail')
 ]
