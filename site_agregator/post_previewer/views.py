95d6eb6ca437ae15670bc2141e6a8fe8771dcfd3
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-15 12:54:54 +0300

    feature: added simple search bar

--- site_agregator/post_previewer/views.py
+++ site_agregator/post_previewer/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic.list import ListView
 from django.views.generic import View
 
+from tagging.models import TaggedItem
+
 from site_parser.models import Post
 
 
@@ -9,6 +11,17 @@ class PostListView(ListView):
     model = Post
     paginate_by = 20
     template_name = 'post_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ListView, self).get_context_data(**kwargs)
+        return context
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return TaggedItem.objects.get_by_model(Post, query)  # ['house', 'thing'])
+        else:
+            return Post.objects.all()
 
 
 class PostDetail(View):
