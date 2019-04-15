ffb592fcb81293a2927bfaebb1bfd1f63557e564
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-15 15:47:09 +0300

    fix: query string tag by id

--- site_agregator/post_previewer/urls.py
+++ site_agregator/post_previewer/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, re_path
 
-from tagging.views import TaggedObjectList
 from site_parser.models import Post
 
 from . import views
@@ -8,5 +7,5 @@ from . import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('<int:pk>', views.PostDetail.as_view(), name='post_detail'),
-    path('tag/<str:tag>', views.PostTagDetail.as_view(), name='post_tag_detail')
+    path('tag/<int:tag>', views.PostTagDetail.as_view(), name='post_tag_detail')
 ]
