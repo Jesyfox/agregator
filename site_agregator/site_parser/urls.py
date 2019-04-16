ef72a984c1f869b30280427c2c5d56eacad06813
Bogdan.S <jesyfoxbs@gmail.com>
2019-04-16 08:57:36 +0300

    feature: refactored parser views to class based views

--- site_agregator/site_parser/urls.py
+++ site_agregator/site_parser/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('habr/', views.habr_parser_starter, name='habr_parser_starter'),
-    path('tproger_python/', views.tproger_python_parser_starter, name='tproger_python_parser_starter'),
+    path('habr/', views.HabrStarter.as_view(), name='habr_parser_starter'),
+    path('tproger_python/', views.TprogerPythonStarter.as_view(), name='tproger_python_parser_starter'),
 ]
--- site_agregator/site_parser/views.py
+++ site_agregator/site_parser/views.py
@@ -1,22 +1,33 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 from .models import Post
 from .tasks import parse
 
 
-def habr_parser_starter(request):
+class ParserView(View):
+    site = ''
+    parser = ''
+    template_name = 'parser_starter.html'
+
+    def __init__(self):
+        super().__init__()
+        self.context = {'models_count': Post.objects.filter(site_url__url=self.site).count(),
+                        'site': self.site}
+
+    def get(self, request):
+        return render(request, self.template_name, context=self.context)
+
+    def post(self, request):
+        parse.delay(self.parser)
+        return render(request, self.template_name, context=self.context)
+
+
+class HabrStarter(ParserView):
     site = 'https://habr.com'
-    context = {'models_count': Post.objects.filter(site_url__url=site).count(),
-               'site': site}
-    if request.method == 'POST':
-        parse.delay('habr')
-    return render(request, 'parser_starter.html', context=context)
+    parser = 'habr'
 
 
-def tproger_python_parser_starter(request):
+class TprogerPythonStarter(ParserView):
     site = 'https://tproger.ru/tag/python/'
-    context = {'models_count': Post.objects.filter(site_url__url=site).count(),
-               'site': site}
-    if request.method == 'POST':
-        parse.delay('tproger-python')
-    return render(request, 'parser_starter.html', context=context)
+    parser = 'tproger-python'
