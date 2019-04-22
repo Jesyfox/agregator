7657b009e1b11c4f07e04b781f3a1f59a328431e
Jesyfox <jesyfoxbs@gmail.com>
2019-04-22 11:31:41 +0100

    fix: List view migration problem

--- site_agregator/post_previewer/views.py
+++ site_agregator/post_previewer/views.py
@@ -15,8 +15,31 @@ class Index(View):
 
 
 class TagList(ListView):
-    queryset = Tag.objects.usage_for_model(Post, counts=True, min_count=2)
+    model = Tag
     template_name = 'tag_list.html'
+
+    def get_queryset(self):
+        if self.queryset is not None:
+            queryset = self.queryset
+            if isinstance(queryset, QuerySet):
+                queryset = queryset.all()
+        elif self.model is not None:
+            queryset = self.model.objects.usage_for_model(Post, counts=True, min_count=2)
+        else:
+            raise ImproperlyConfigured(
+                "%(cls)s is missing a QuerySet. Define "
+                "%(cls)s.model, %(cls)s.queryset, or override "
+                "%(cls)s.get_queryset()." % {
+                    'cls': self.__class__.__name__
+                }
+            )
+        ordering = self.get_ordering()
+        if ordering:
+            if isinstance(ordering, str):
+                ordering = (ordering,)
+            queryset = queryset.order_by(*ordering)
+
+        return queryset
 
 
 class PostTagDetail(TaggedObjectList):
