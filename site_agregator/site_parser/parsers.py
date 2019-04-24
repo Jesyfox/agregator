4636c9c1703babef058927337f8133f764f226f1
Jesyfox <jesyfoxbs@gmail.com>
2019-04-24 16:14:15 +0100

    fix: moved add_post_to_db from utils.py to parsers.py

--- site_agregator/site_parser/parsers.py
+++ site_agregator/site_parser/parsers.py
@@ -1,9 +1,30 @@
+from django.db.utils import IntegrityError
+
 import logging
 import requests
 import lxml.html as html
 import time
 
 from . import utils
+from .models import Post
+
+logger = logging.getLogger('parser')
+
+
+def add_post_to_db(data):
+    new_post = Post(site_url=utils.get_site_url_object(data['site_url']),
+                    post_url=data['post_url'],
+                    title=data['title'],
+                    author=utils.get_author_object(data['author']),
+                    body_content=data['body_content']
+                    )
+    try:
+        new_post.save()
+    except IntegrityError:
+        pass
+    else:
+        new_post.tags = ','.join(data['tags']).lower()
+        logger.info(f'new_post: {data["title"]}')
 
 
 class Parser:
@@ -25,12 +46,10 @@ class Parser:
         if not self.site_url:
             raise NotImplementedError('You must overwrite site_url var')
 
-        self.logger = logging.getLogger('parser')
-
         try:
             response = requests.get(self.site_url, timeout=20)
         except requests.exceptions.Timeout:
-            self.logger.error(f'main url {self.site_url}, timed out!')
+            logger.error(f'main url {self.site_url}, timed out!')
         else:
             self.page_tree = self.get_tree_element(response)
 
@@ -57,40 +76,40 @@ class Parser:
 
     def tree_iterator(self, url_list):
         for url in url_list:
-            self.logger.debug(f'get url {url}')
+            logger.debug(f'get url {url}')
             try:
                 response = requests.get(url, timeout=20)
                 time.sleep(3)
             except requests.exceptions.Timeout:
-                self.logger.error(f'url {url}, timed out!')
+                logger.error(f'url {url}, timed out!')
             else:
                 tree = self.get_tree_element(response, base_url=url)
-                self.logger.debug('yielding tree')
+                logger.debug('yielding tree')
                 yield tree
 
     def parse_data_from_post(self, post_tree):
         data = {'site_url': self.site_url}
-        self.logger.debug('parsing data from post...')
+        logger.debug('parsing data from post...')
         for field_key in self.field_mappings.keys():
             try:
-                self.logger.debug(f'getting field key {field_key}')
+                logger.debug(f'getting field key {field_key}')
                 field = getattr(self, self.field_mappings[field_key])
                 data.update({field_key: field(post_tree)})
-                self.logger.debug('field key added')
+                logger.debug('field key added')
             except Exception as e:
-                self.logger.error(e)
+                logger.error(e)
                 continue
         data.update({'post_url': post_tree.base})
-        self.logger.debug('page_parsing done!')
+        logger.debug('page_parsing done!')
         return data
 
     def start_parse(self):
-        self.logger.debug('start parse...')
+        logger.debug('start parse...')
         for page_tree in self.tree_iterator(self._get_page_list()):
             for post_tree in self.tree_iterator(self._get_post_list(page_tree)):
                 data = self.parse_data_from_post(post_tree)
-                utils.add_post_to_db(data)
-        self.logger.debug('parse is done!')
+                add_post_to_db(data)
+        logger.debug('parse is done!')
 
     @classmethod
     def register(cls, name):
--- site_agregator/site_parser/utils.py
+++ site_agregator/site_parser/utils.py
@@ -1,9 +1,4 @@
-from django.db.utils import IntegrityError
-from .models import Post, SiteUrl, Author
-
-import logging
-
-logger = logging.getLogger('parser')
+from .models import SiteUrl, Author
 
 
 def get_author_object(author):
@@ -12,19 +7,3 @@ def get_author_object(author):
 
 def get_site_url_object(site_url):
     return SiteUrl.objects.get_or_create(url=site_url)[0]
-
-
-def add_post_to_db(data):
-    new_post = Post(site_url=get_site_url_object(data['site_url']),
-                    post_url=data['post_url'],
-                    title=data['title'],
-                    author=get_author_object(data['author']),
-                    body_content=data['body_content']
-                    )
-    try:
-        new_post.save()
-    except IntegrityError:
-        pass
-    else:
-        new_post.tags = ','.join(data['tags']).lower()
-        logger.info(f'new_post: {data["title"]}')
